6e1b6b5c8ec14ed098f18f3a00f80c8dcc105e17
Chase Ellsworth <chaseme3@gmail.com>
2014-12-04 12:25:17 -0800

    connects to mongodb database

--- server/server.js
+++ server/server.js
@@ -3,19 +3,26 @@ var app = express();
 var mongoose = require('mongoose');	
 var bodyParser = require('body-parser');
 var path = require('path');
-
 var dir = path.resolve(__dirname + '/../client');
 var morgan = require('morgan');
 var voteRouter = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
-// mongoose.connect()
+mongoose.connect('mongodb://localhost/voteDatabase')
+
+///////to see if it's connected to a database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log('hurrraaaaay!!!');
+});
+//////
 
 app.use(morgan('dev'));
 app.use(express.static(dir));
 
-// app.use('some route', some Router);
+app.use('/api', voteRouter);
 
 var server = app.listen(3000, function(){
 
@@ -27,5 +34,5 @@ var server = app.listen(3000, function(){
 })
 
 
-// require('some router path')('the name of the router');
+require('./routes')(voteRouter);
 
