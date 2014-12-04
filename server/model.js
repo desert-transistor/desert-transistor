f2a9df59111d15582f1f12abd98fedafacea48fc
Chase Ellsworth <chaseme3@gmail.com>
2014-12-04 12:25:17 -0800

    connects to mongodb database

--- server/model.js
+++ server/model.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 
 var confusionSchema = new mongoose.Schema({
-		lectureID:  "lectureID",
-		createdAt:  "createdAt",
-		studentID:  "studentID"
+		
+		lectureID:  String,
+		createdAt:  String,
+		studentID:  String
+
 	});
 
 module.exports = mongoose.model('confusionDB', confusionSchema);
