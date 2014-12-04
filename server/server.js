0670e6967f2bb6268f99d9213f73c09f30588015
Nick Mandel <nrmandel@ucdavis.edu>
2014-12-04 12:32:54 -0800

    adds socket server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+
 var mongoose = require('mongoose');	
 var bodyParser = require('body-parser');
 var path = require('path');
-
 var dir = path.resolve(__dirname + '/../client');
 var morgan = require('morgan');
+
+var socketServer = require('http').Server(app);
+var io = require('socket.io')(socketServer);
+
+io.on('connection', function(socket) {
+	socket.emit('event', {it : 'works'});
+})
 // var someRouter = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -16,6 +23,10 @@ app.use(morgan('dev'));
 app.use(express.static(dir));
 
 // app.use('some route', some Router);
+// app.get("/", function(req, res) {
+// 	res.sendfile('../client/student.html');
+// });
+app.use(express.static(__dirname + '/client'));
 
 var server = app.listen(3000, function(){
 
