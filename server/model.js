b237d6fee0375a423046e4ca1b4b7b7a03f27584
Chase Ellsworth <chaseme3@gmail.com>
2014-12-04 11:47:54 -0800

    adds server model

--- server/model.js
+++ server/model.js
@@ -1,7 +1,9 @@
+var mongoose = require('mongoose');
 
-Schema
-	{
-		lectureID
-		createdAt
-		studentID
-	}
+var confusionSchema = new mongoose.Schema({
+		lectureID:  "lectureID",
+		createdAt:  "createdAt",
+		studentID:  "studentID"
+	});
+
+module.exports = mongoose.model('confusionDB', confusionSchema);
