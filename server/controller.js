2ecae3aa88d079db6159d47ff22b8a56b096b7ef
Chase Ellsworth <chaseme3@gmail.com>
2014-12-04 11:59:36 -0800

    adds server controller

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,35 @@
-controller.js
+var confusionModel = require('./model.js');
+var path = require('path');
+var Q = require('q');
+
+module.exports = {
+
+	addVotes: function(req, res, next){
+		confusionModel.create(req.body, function(error, data){
+			console.log('@@@@@@@@@@@@@@');
+			console.log(req.body);
+			if(error){
+				console.log('error');
+				res.send(500);
+			}else{
+				console.log('success');
+				res.send(data);
+			}
+		})
+	},
+
+	getVotes: function(req, res, next){
+		confusionModel.find( function(error, data){
+			console.log('$$$$$$$$$$$$$$$');
+			console.log(req.body);
+			if(error){
+				console.log('error');
+				res.send(500);
+			}else{
+				console.log('success');
+				res.send(data);
+			}
+		})
+	}
+
+}
