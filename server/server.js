36cb4f6fbfe76827b51c2f0dc172318da9703002
Nick Mandel <nrmandel@ucdavis.edu>
2014-12-04 16:01:19 -0800

    implements/refactors socket communication between server and client

--- server/server.js
+++ server/server.js
@@ -1,42 +1,29 @@
 var express = require('express');
 var app = express();
-
-var mongoose = require('mongoose');	
-var bodyParser = require('body-parser');
 var path = require('path');
 var dir = path.resolve(__dirname + '/../client');
+
+var mongoose = require('mongoose');  
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
-var socketServer = require('http').Server(app);
-var io = require('socket.io')(socketServer);
-
-io.on('connection', function(socket) {
-	socket.emit('event', {it : 'works'});
-})
-// var someRouter = express.Router();
-
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-// mongoose.connect()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit("event", {it: "works"});
+});
+
 app.use(express.static(dir));
 
-// app.use('some route', some Router);
-// app.get("/", function(req, res) {
-// 	res.sendfile('../client/student.html');
-// });
-app.use(express.static(__dirname + '/client'));
-
-var server = app.listen(3000, function(){
-
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Student Confusion app is listening at http://%s:%s', host, port);
-
-})
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 
-// require('some router path')('the name of the router');
 
