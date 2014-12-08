faced1f353cdcbaf7e75da23fab715985093eb42
Chase Ellsworth <chaseme3@gmail.com>
2014-12-08 10:50:15 -0800

    cleans up index.html

--- client/socket.js
+++ client/socket.js
@@ -1,16 +1,16 @@
-var socket = io();
+// var socket = io();
 
-console.log("getting app.js");
+// console.log("getting app.js");
 
-socket.on('connect', function(){
-	console.log("socket connected");
-});
+// socket.on('connect', function(){
+// 	console.log("socket connected");
+// });
 
-socket.on('event', function(data){
-	console.log("data: ");
-	console.log(data);
-});
+// socket.on('event', function(data){
+// 	console.log("data: ");
+// 	console.log(data);
+// });
 
-socket.on('disconnect', function(){
-	console.log("socket DISconnected");
-});
+// socket.on('disconnect', function(){
+// 	console.log("socket DISconnected");
+// });
