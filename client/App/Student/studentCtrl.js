ae2e598a2e4d04f1839427395289e116fa01dc6c
Chase Ellsworth <chaseme3@gmail.com>
2014-12-08 10:05:24 -0800

    updates student controller

--- client/App/Student/studentCtrl.js
+++ client/App/Student/studentCtrl.js
@@ -1,1 +1,13 @@
-studentCtrl.js
+
+angular
+	  .module('app')
+	  .controller('StudentController', studentController)
+
+StudentController.$inject = ['$scope', 'studentFactory'];
+
+function StudentController($scope, studentFactory){
+
+	$scope.connect = studentFactory.connect;
+
+	$scope.confusedStudent = studentFactory.confusedStudent;
+}
--- client/App/Student/studentFactory.js
+++ client/App/Student/studentFactory.js
@@ -1,1 +1,54 @@
-studentFactory.js
+angular
+	  .module('app')
+	  .factory('studentFactory', studentFactory)
+
+		var socket = io();
+
+		console.log("getting app.js");
+
+		function connect (){
+			socket.on('connect', function(){
+				console.log("socket connected");
+			});
+		
+			socket.on('disconnect', function(){
+				console.log("socket DISconnected");
+			});
+
+			socket.on('event', function(data){
+				console.log("data: ");
+				console.log(data);
+			});
+		}
+
+		function confusedStudent (){
+
+			socket.emit("confusion", {
+				lectureID: "RECURSION",
+				studentID: "THOMAS"
+			}); 
+			console.log("I done got clicked.");
+		}
+
+
+		
+		// document.getElementById('confused').addEventListener('click', function() {
+		// 	socket.emit("confusion", {
+		// 		lectureID: "RECURSION",
+		// 		studentID: "THOMAS"
+		// 	}); 
+		// 	console.log("I done got clicked.");
+		// })
+
+		// socket.on('connect', function(){
+		// 	console.log("socket connected");
+		// });
+
+		// socket.on('event', function(data){
+		// 	console.log("data: ");
+		// 	console.log(data);
+		// });
+
+		// socket.on('disconnect', function(){
+		// 	console.log("socket DISconnected");
+		// });
